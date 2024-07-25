fb585eb2d15738e92b4c4da0b08393d9b1860dc6
Squashedapricot <shriss999@protonmail.com>
2024-07-25 15:09:34 +0530

    done with hello and integer

--- hello/main.go
+++ hello/main.go
@@ -4,26 +4,34 @@ import (
 	"fmt"
 )
 
-const spanish = "Spanish"
-const french = "French"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 func main() {
-	fmt.Println(Hello("Chris", "English"))
+	fmt.Println(Hello("Elloit", "French"))
 }
 
 func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+
 	switch language {
 	case spanish:
 		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return prefix + name
+	return
 }
