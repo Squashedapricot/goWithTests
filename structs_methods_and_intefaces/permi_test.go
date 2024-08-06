0a9ef8549d5d2b14923f80401b0630372be1a33b
squashedapricot <shriss999@protonmail.com>
2024-08-06 16:31:54 +0530

    dne Struct,methods,interface revisit necessary

--- structs_methods_and_intefaces/permi_test.go
+++ structs_methods_and_intefaces/permi_test.go
@@ -21,7 +21,7 @@ func TestPerimeter(t *testing.T) {
 	t.Run("circle", func(t *testing.T) {
 		circle := Circle{10.0}
 		got := circle.Perimeter()
-		want := math.Pi * 10.0 * 10.0
+		want := 2 * math.Pi * 10.0
 		if got != want {
 			t.Errorf("expected %g got %g", want, got)
 		}
@@ -29,16 +29,20 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("expected %g got %g", want, got)
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{length: 10.0, breadth: 10.0}, hasArea: 100.0},
+		{name: "Circle", shape: Circle{radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("got %g want %g", got, tt.hasArea)
 		}
 	}
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		want := 100.0
-		checkArea(t, rectangle, want)
-	})
 }
