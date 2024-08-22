f5b80655515896e859f76fd1cac042653523d54f
Squashedapricot <shriss999@protonmail.com>
2024-08-22 21:07:26 +0530

    done with withdraw function and refactor

--- pointers_and_errors/wallet.go
+++ pointers_and_errors/wallet.go
@@ -1,19 +1,29 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type Bitcoin int
 
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
-
 }
 
-func (w *Wallet) Balance() int {
-	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) withDraw(amount Bitcoin) {
+	w.balance -= amount
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
